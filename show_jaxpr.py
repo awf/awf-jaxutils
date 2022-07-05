3bbf96853232ccda7e0054715b119b77927c3ed8
Andrew Fitzgibbon <awf@graphcore.ai>
2022-07-05 16:26:01 +0100

    Simplified, add roundtripping test

--- show_jaxpr.py
+++ show_jaxpr.py
@@ -4,19 +4,22 @@ import jax
 import sys
 import re
 
+
 def cat(xs):
     return "".join(xs)
 
 
-def intercomma(xs):
+def intercomma(*xs):
     return ", ".join(xs)
 
 
-def intercommastr(xs):
+def intercommastr(*xs):
     return ", ".join((str(x) for x in xs))
 
-def intercommavars(xs):
+
+def intercommavars(*xs):
     return ", ".join((varstr(x) for x in xs))
+
 
 def justone(iter):
     l = list(iter)
@@ -41,10 +44,14 @@ def doc_from_source_line(source_info):
 
 foo_num = 1000
 
+
 def pythonize(name):
-    name = re.sub('-','_',name)
-    assert re.match('[a-zA-Z0-9_]+', name)
-    return name
+    new_name = re.sub("-", "_", name)
+    assert re.match("[a-zA-Z0-9_]+", new_name)
+    if new_name != name and name != "scatter-add":
+        print(f"Note: pythonize converted {name} to {new_name}")
+    return new_name
+
 
 def new_name(base):
     global foo_num
@@ -52,28 +59,51 @@ def new_name(base):
     foo_num += 1
     return n
 
+
 def varstr(x):
-    if isinstance(x, (jax.core.DropVar, jax.core.Literal, tuple, type(None), 
-                        jax.numpy.dtype,jax.lax.GatherScatterMode)):
+    if isinstance(
+        x,
+        (
+            jax.core.DropVar,
+            jax.core.Literal,
+            tuple,
+            type(None),
+            jax.numpy.dtype,
+            jax.lax.GatherScatterMode,
+        ),
+    ):
         return str(x)
 
     if isinstance(x, jax.core.Var):
-        return str(x) + '_'
+        return str(x) + "_"
 
-    if isinstance(x, (str, bool, int)):
-        return repr(x)
+    if isinstance(x, (jax.lax.GatherDimensionNumbers,)):
+        return "GatherDimensionNumbers" + repr(x)
 
     if isinstance(x, (types.FunctionType)):
-        return x.__module__ + '.' + x.__name__
+        return x.__module__ + "." + x.__name__
 
-    assert False, f"Check this shouldn't be transformed [{repr(x)}]"
-    return str(x)
+    if x is np.float32:
+        return "float32"
+    if x is np.int32:
+        return "int32"
+
+    # This check just to ensure we have eyeballed all cases that need to be 'repr'ed
+    assert isinstance(
+        x, (str, bool, int, jax.lax.GatherDimensionNumbers)
+    ), f"Check this shouldn't be transformed [{repr(x)}]"
+
+    return repr(x)
+
 
 def pytype(x):
     if isinstance(x, jax.ShapedArray):
-        return f'ShapedArray({x.shape}, {x.dtype}, {x.weak_type}, {repr(x.named_shape)})'
+        return (
+            f"ShapedArray({x.shape}, {x.dtype}, {x.weak_type}, {repr(x.named_shape)})"
+        )
 
-    return 'Any'
+    return "Any"
+
 
 # TODO:
 #             bdy_ = custom_jvp_call_jaxpr(custom_jvp_call_jaxpr1013)(bdx_, jvp_jaxpr_thunk=<function _memoize.<locals>.memoized at 0x7f8a645d2af0>, num_consts=0)
@@ -82,78 +112,115 @@ def pytype(x):
 #             ok_ = scatter-add(oj_,oc_,oi_, update_jaxpr={ [34m[22m[1mlambda [39m[22m[22m; a[35m:f32[39m b[35m:f32[39m. [34m[22m[1mlet[39m[22m[22m c[35m:f32[39m = add a b [34m[22m[1min [39m[22m[22m(c,) }, update_consts=(), dimension_numbers=ScatterDimensionNumbers(update_window_dims=(), inserted_window_dims=(0, 1, 2), scatter_dims_to_operand_dims=(0, 1, 2)), indices_are_sorted=True, unique_indices=True, mode=GatherScatterMode.PROMISE_IN_BOUNDS)
 #             def xla_call1003(a_: ShapedArray(float32[128,32,512]),b_: ShapedArray(int32, weak_type=True)):
 
+from jax import lax
+import jax.numpy as jnp
+from icecream import ic
+
 
 def examine_jaxpr(f, jaxpr, *, indent="", doc="", file=sys.stdout):
-    args = intercomma((varstr(v) + f": {pytype(v.aval)}" for v in jaxpr.invars))
+    args = intercomma(*(varstr(v) + f": '{pytype(v.aval)}'" for v in jaxpr.invars))
     print(f"\n{indent}def {f}({args}):", file=file)
     indent += tab
     if doc:
         print(f'{indent}"""{doc}"""', file=file)
+
     for cv in jaxpr.constvars:
-        assert cv not in ['if', 'in', 'is'] # if it is, use varstr(cv) on next line
+        assert cv not in ["if", "in", "is"]  # if it is, use varstr(cv) on next line
         print(f"{indent}{cv} = ?", file=file)
 
     for eqn in jaxpr.eqns:
-        sub_jaxpr = None
-        if eqn.primitive.name == "custom_jvp_call_jaxpr":
-            sub_jaxpr = "fun_jaxpr"
-            cj = eqn.params[sub_jaxpr].jaxpr
-            pass
-        if eqn.primitive.name == "scan":
-            sub_jaxpr = "jaxpr"
-            cj = eqn.params[sub_jaxpr].jaxpr
-            pass
-        if eqn.primitive.name == "xla_pmap":
-            sub_jaxpr = "call_jaxpr"
-            cj = eqn.params[sub_jaxpr]
-            pass
-        if eqn.primitive.name == "scatter-add":
-            sub_jaxpr = "update_jaxpr"
-            cj = eqn.params[sub_jaxpr]
-            pass
-        if eqn.primitive.call_primitive:
-            sub_jaxpr = "call_jaxpr"
-            cj = eqn.params[sub_jaxpr]
-            pass
 
-        if sub_jaxpr:
-            pyname = pythonize(eqn.primitive.name)
-            n = new_name(pyname)
-            primname = f'{pyname}({n})'
+        ## Recursively dump sub-jaxprs, and add references to params
+        new_params = {}
+        for key, val in eqn.params.items():
+            if isinstance(val, tuple):
+                assert not any(
+                    isinstance(x, (jax.core.Jaxpr, jax.core.ClosedJaxpr)) for x in val
+                ), "Don't expect sub_jaxprs in tuples"
+
+            # Special cases: scatter-add
+            if eqn.primitive is lax.scatter_add_p and key in (
+                "update_jaxpr",
+                "update_consts",
+            ):
+                continue
+
+            if isinstance(val, jax.core.Jaxpr):
+                sub_jaxpr = val
+            elif isinstance(val, jax.core.ClosedJaxpr):
+                sub_jaxpr = val.jaxpr
+            else:
+                # No sub_jaxpr - just update new_params and continue
+                new_params[key] = val
+                continue
+
+            # Sub-jaxpr, make a name, and recurse
+            n = new_name(pythonize(eqn.primitive.name))
 
             doc = doc_from_source_line(eqn.source_info)
-            examine_jaxpr(n, cj, indent=indent, doc=doc, file=file)
-            params = intercomma(
-                f"{n}={varstr(v)}" for (n, v) in eqn.params.items() if n != sub_jaxpr
-            )
+            examine_jaxpr(n, sub_jaxpr, indent=indent, doc=doc, file=file)
+
+            new_params[key] = jax.core.Literal(n, None)
+
+        primname = eqn.primitive.name + "_p.bind"
+        if eqn.primitive is jax.interpreters.xla.xla_call_p:
+            # Handle xla_call specially - essentially erase it.  TODO: do we ever need to preserve the xla_call annotations?
+            callee = new_params["call_jaxpr"]
+            translation = f"{callee}({intercommavars(*eqn.invars)}) # {new_params}"
 
         else:
-            params = intercomma(f"{n}={varstr(v)}" for (n, v) in eqn.params.items())
-            primname = pythonize(str(eqn.primitive))
+            if eqn.primitive is lax.scatter_add_p:
+                primname = "scatter_add"
 
-        if eqn.invars:
-            params = intercommavars(eqn.invars) + (', ' + params if params else '')
+            bind_args = intercomma(
+                *(f"{varstr(v)}" for v in eqn.invars),
+                *(f"{n}={varstr(v)}" for (n, v) in new_params.items()),
+            )
 
-        print(
-            f"{indent}{intercommavars(eqn.outvars)} = {primname}({params})", 
-            file=file
-        )
+            translation = f"{primname}({bind_args})"
 
-    print(f"{indent}return ({intercommavars(jaxpr.outvars)})", file=file)
+        print(f"{indent}{intercommavars(*eqn.outvars)} = {translation}", file=file)
+
+    print(f"{indent}return ({intercommavars(*jaxpr.outvars)})", file=file)
 
 
-def show_jaxpr(f, args, file=sys.stdout, **kwargs):
+def show_jaxpr(f, args, name=None, file=sys.stdout, **kwargs):
     """
     Show jaxpr f as if in python, i.e. "decompile" to python
     """
-    print(f"\n# show_jaxpr", file=file)
+
+    print(
+        f"""
+# show_jaxpr {f}
+from jax.lax import *
+import jax.numpy as jnp
+from numpy import float32,int32
+
+add_any_p = add_p
+
+DeviceArray = jnp.array
+""",
+        file=file,
+    )
+
+    if name is None:
+        name = f.__name__
+
     closed_jaxpr = jax.make_jaxpr(f, **kwargs)(*args)
-    examine_jaxpr(f.__name__, closed_jaxpr.jaxpr, doc=f.__doc__, file=file)
+    examine_jaxpr(name, closed_jaxpr.jaxpr, doc=f.__doc__, file=file)
+
+    print(
+        f"""
+if __name__ == '__main__':
+    {name}{args}
+""",
+        file=file,
+    )
 
 
 def show_xla(f, args, file=sys.stdout, **kwargs):
     """
-    Show XLA for f, using template args 
+    Show XLA for f, using template args
     """
     xla = jax.xla_computation(f, **kwargs)(*args)
     print("XLA=", xla.as_hlo_text(), file=file)
@@ -165,22 +232,61 @@ def show_jaxpr_and_xla(f, args, file=sys.stdout, **kwargs):
 
 
 if __name__ == "__main__":
-    def f(p,x,q):
-        for i in range(5):
-            x = jax.numpy.matmul(x,i*p*x)
+
+    def foo(p, x, q):
+        x = jax.numpy.matmul(x, p * x.T)
         return (x + x[3]).std()
 
-    f = jax.grad(f, argnums=1)
-    f = jax.vmap(f, in_axes=(None,2,None))
-    
-    args = (2.2, jax.numpy.ones((3,3,5)), 'q')
-    show_jaxpr_and_xla(f, args)
+    gradf = jax.grad(foo, argnums=1)
+    vmapgradf = jax.vmap(gradf, in_axes=(None, 2, None))
 
-    fjit = jax.jit(f, static_argnums=(0))
-    show_jaxpr_and_xla(fjit, args, static_argnums=(0))
+    f = vmapgradf
 
-    with open("/tmp/show_jaxpr.jaxpr.py", "w") as file:
-        show_jaxpr(f, args, file=file)
+    prng = jax.random.PRNGKey(42)
+    args = (2.2, jax.random.normal(prng, (3, 2, 5)), "q")
+    print(f(*args))
+
+    # Save to file
+    fn = "tmp/show_jaxpr_jaxpr.py"
+    with open(fn, "w") as file:
+        show_jaxpr(f, args, name="f", file=file)
 
     import os
-    os.system("ls -lrt /tmp/show_jaxpr.jaxpr.py")
+
+    os.system(f"black {fn}")
+
+    # Load from file
+    import importlib.util
+
+    module_name = "show_jaxpr_roundtrip"
+    spec = importlib.util.spec_from_file_location(module_name, fn)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+    # Check rountrip: does module.f give the same result?
+    print(module.f(*args) - f(*args))
+
+    # Save again
+    fn2 = "tmp/show_jaxpr_roundtrip.py"
+    with open(fn2, "w") as file2:
+        show_jaxpr(module.f, args, file=file2)
+
+    os.system(f"black {fn2}")
+
+    # Could diff fn and fn2 now.
+
+    # Reload for 2nd roundtrip
+    module_name = "show_jaxpr_roundtrip2"
+    spec = importlib.util.spec_from_file_location(module_name, fn2)
+    module2 = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module2
+    spec.loader.exec_module(module2)
+    print(module2.f(*args) - f(*args))
+
+    # Sand save 2nd roundtrip
+    fn3 = "tmp/show_jaxpr_roundtrip2.py"
+    with open(fn3, "w") as file3:
+        show_jaxpr(module2.f, args, file=file3)
+
+    os.system(f"black {fn3}")
